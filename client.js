29c78047f6ce1e6eafc57a61720f150ba90251f5
curriecode <melaniecurrie01@gmail.com>
2019-09-25 15:22:31 -0700

    "port and host loctaed in constants.js, everything working"

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
+
 
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.on('connect', () => {
     console.log("You're connected to the game server!");
