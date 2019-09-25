a0313533e2137b9e4d711469e6d390eac437f5d5
curriecode <melaniecurrie01@gmail.com>
2019-09-25 14:29:21 -0700

    "handleUser input is set up ctrl/c is working"

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+
 const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
@@ -7,7 +8,9 @@ const connect = function () {
   conn.on('connect', () => {
     console.log("You're connected to the game server!");
     conn.write("Name: Mel");
+    conn.write("Move: up");
   });
+
   conn.on('data', () => {
     console.log('You dedd cause you idled');
   });
@@ -16,3 +19,9 @@ const connect = function () {
 };
 
 module.exports = connect;
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
